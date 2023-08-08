56fee5914aaea103a97be3c887d104a5e95ae4d6
Morotem <nathan.hvx@live.fr>
2023-08-08 16:45:24 +0200

    model in web canvas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, Suspense, useState, useEffect } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
-import { useGLTF } from "@react-three/drei";
+import { PerspectiveCamera, useGLTF } from "@react-three/drei";
 import { VRCanvas, ARCanvas } from "react-xr";
 import { Grid, Switch, Button } from "@mui/material/";
 import { OrbitControls } from '@react-three/drei'
+import { SecondModifiedBatiment } from "./SecondModifiedBatiment";
+import "./AppStyle.css"; // Import the CSS file
 
 function Model(props) {
-  const { scene } = useGLTF("/batiment.gltf");
+  const { scene } = useGLTF("/SecondModifiedBatiment.gltf");
 
   return (
     <primitive
       object={scene}
       scale={props.scale || [0.1, 0.1, 0.1]}
+      position={props.position || [0, 0, 5]}
       onClick={props.onClick}
     />
   );
@@ -36,12 +39,26 @@ function Sphere() {
 function App() {
   const [showCube, setShowCube] = useState(true);
   const [mode, setMode] = useState("vr");
+  const [isAutoRotating, setIsAutoRotating] = useState(true);
+  const [isXrSupported, setIsXrSupported] = useState(false);
 
   const handleSceneModeChange = (event) => {
     setMode(event.target.checked ? "ar" : "vr");
   };
+  const handleAutoRotateChange = (event) => {
+    setIsAutoRotating(event.target.checked);
+  };
 
   const XRCanvas = mode === "vr" ? VRCanvas : ARCanvas;
+
+  useEffect(() => {
+    async function checkXRSupport() {
+      const supported = await navigator.xr.isSessionSupported("immersive-vr");
+      setIsXrSupported(supported);
+    }
+
+    checkXRSupport();
+  }, []);
 
   return (
     <>
@@ -52,36 +69,81 @@ function App() {
         style={{ height: "100vh" }} // Adjust the height of the container to fill the viewport
       >
         <h1>DEMO AR/VR</h1>
+        {!isXrSupported ?
+          <Grid item>
+            <h3>XR is not supported</h3>
+          </Grid>
+          :
+          <Grid item>
+            <Grid container direction="row" spacing={2} alignItems="center">
+              <Grid item>
+                <Switch
+                  checked={mode === "ar"}
+                  onChange={handleSceneModeChange}
+                  name="scene-mode-switch"
+                />
+              </Grid>
 
+
+
+              <Grid item>
+                <label htmlFor="scene-mode-switch">
+                  {mode === "vr" ? "VR Mode" : "AR Mode"}
+                </label>
+              </Grid>
+            </Grid>
+          </Grid>
+        }
         <Grid item>
           <Grid container direction="row" spacing={2} alignItems="center">
             <Grid item>
               <Switch
-                checked={mode === "ar"}
-                onChange={handleSceneModeChange}
-                name="scene-mode-switch"
+                checked={isAutoRotating}
+                onChange={handleAutoRotateChange}
+                name="autorotate-switch"
               />
             </Grid>
+
+
+
             <Grid item>
-              <label htmlFor="scene-mode-switch">
-                {mode === "vr" ? "VR Mode" : "AR Mode"}
+              <label htmlFor="autorotate">
+                "Auto-rotate"
               </label>
             </Grid>
           </Grid>
         </Grid>
 
-        <Grid item style={{ flex: 1, width: "100%" }}>
-          <XRCanvas style={{ height: "80%", width: "100%" }}> 
-            {showCube ? (
-              <Model scale={[0.5, 0.5, 0.5]} />
-            ) : (
-              <Sphere />
-            )}
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <OrbitControls />
-          </XRCanvas>
-        </Grid>
+        <Canvas style={{ background: "white" }}>
+          <ambientLight intensity={1} />
+          <spotLight intensity={1} angle={1.5} penumbra={1} position={[0, 15, 10]} />
+          <spotLight intensity={1} angle={1.5} penumbra={1} position={[0, 15, -10]} />
+          <spotLight intensity={1} angle={1.5} penumbra={1} position={[10, 15, 0]} />
+          <spotLight intensity={1} angle={1.5} penumbra={1} position={[-10, 15, 0]} />
+          <Suspense fallback={null}>
+            <SecondModifiedBatiment />
+          </Suspense>
+          <PerspectiveCamera makeDefault position={[0, 0, 20]} fov={60}>
+            <OrbitControls autoRotate={isAutoRotating} autoRotateSpeed={1} />
+          </PerspectiveCamera>
+        </Canvas>
+
+        {isXrSupported &&
+          <Grid item style={{ flex: 1, width: "100%" }}>
+            <XRCanvas style={{ height: "80%", width: "100%" }}>
+              {showCube ? (
+                <Model scale={[0.5, 0.5, 0.5]} />
+              ) : (
+                <Sphere />
+              )}
+              <ambientLight />
+              <pointLight position={[10, 10, 10]} />
+              
+                <OrbitControls />
+              
+            </XRCanvas>
+          </Grid>
+        }
       </Grid>
     </>
   );
