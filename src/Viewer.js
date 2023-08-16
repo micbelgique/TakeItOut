b45310539967ecb24db018aaf916f041dbc32cd5
Morotem <nathan.hvx@live.fr>
2023-08-16 10:40:48 +0200

    stop autoRotate on touch

--- src/Viewer.js
+++ src/Viewer.js
@@ -27,9 +27,7 @@ function Viewer() {
     const handleSceneModeChange = (event) => {
         setMode(event.target.checked ? "AR" : "VR");
     };
-    const handleAutoRotateChange = (event) => {
-        setIsAutoRotating(event.target.checked);
-    };
+    
 
     useEffect(() => {
         async function checkXRSupport() {
@@ -51,25 +49,7 @@ function Viewer() {
             >
                 <h1>Dimension Swap</h1>
                 
-                <Grid item>
-                    <Grid container direction="row" alignItems="center">
-                        <Grid item style={{ width: "10vh" }}>
-                            <Switch
-                                checked={isAutoRotating}
-                                onChange={handleAutoRotateChange}
-                                name="autorotate-switch"
-                            />
-                        </Grid>
-
-
-
-                        <Grid item style={{ width: "15vh" }}>
-                            <label htmlFor="autorotate">
-                                Auto-rotate
-                            </label>
-                        </Grid>
-                    </Grid>
-                </Grid>
+                
 
                 <Grid item style={{ flex: 1, width: "100%" }}>
                     {(mode === "AR") &&
@@ -79,12 +59,7 @@ function Viewer() {
                             }} />
                             <Canvas >
                                 <XR referenceSpace="local">
-                                    <OrbitControls autoRotate={isAutoRotating} />
-                                    <ambientLight intensity={1} />
-                                    <spotLight intensity={1} angle={1.5} penumbra={1} position={[0, 15, 10]} />
-                                    <spotLight intensity={1} angle={1.5} penumbra={1} position={[0, 15, -10]} />
-                                    <spotLight intensity={1} angle={1.5} penumbra={1} position={[10, 15, 0]} />
-                                    <spotLight intensity={1} angle={1.5} penumbra={1} position={[-10, 15, 0]} />
+                                    
                                     <XrHitModel modelUrl={modelUrl} scale={scale}/>
 
                                 </XR>
